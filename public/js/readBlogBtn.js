03ff8a2cc4cfb560ebdf7f7b53936672d304c73c
Kishan <kishanrajrby2@gmail.com>
2024-06-05 18:34:28 +0530

    error fixing in blogread

--- public/js/readBlogBtn.js
+++ public/js/readBlogBtn.js
@@ -8,10 +8,11 @@ if (closebtn) {
 }
 
 let flagIcon = document.querySelector('#imagecode');
-let countryCode = document.querySelector('#outputCountry').textContent;
+let countryCode = document.querySelector('#outputCountry').textContent.trim();
 let val = countryName[countryCode].code.trim();
 let scrs = `https://flagsapi.com/${val}/flat/64.png`;
 flagIcon.src = scrs;
 
 let countriesName = document.querySelector('#outputCountry');
-countriesName.innerText=countryName[countryCode].name.trim()
+countriesName.innerText=countryName[countryCode].name.trim();
+console.log(countryName)
